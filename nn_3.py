17be2e81558582b9e988dcf40b0d5f1033872e17
Damien_Machado <47127792+Damien-Machado@users.noreply.github.com>
2020-03-17 19:05:38 -0700

    Update nn_3.py

--- nn_3.py
+++ nn_3.py
@@ -1,19 +1,12 @@
 import pandas
 import math
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras.models import Model
 from sklearn.preprocessing import label_binarize
 from sklearn.model_selection import train_test_split
-
-from sklearn.ensemble import ExtraTreesClassifier as XTC
-
-
-# this part is just straight-up Emily's code
+from sklearn.ensemble import ExtraTreesClassifier as ERT
 
 # read in the data
 print("reading in data!")
 data = pandas.read_csv('train.csv')
-
 
 # Split into x (features) and y (target info)
 x = data.drop('Cover_Type', axis=1).values
@@ -23,33 +16,12 @@ y = label_binarize(data[['Cover_Type']], classes)
 
 # set aside a test set
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.05)
-
 print("been split, setting up network now")
 
-# this part is (roughly) from assignment 2
-'''
-# set up the neural network
-input_layer = Input(shape=(55,))
-
-# add dropout here somehow
-x0 = Dense(200, activation='relu', input_shape=(55,))(input_layer)
-x1 = Dense(100, activation='relu')(x0)
-x2 = Dense(50, activation='relu')(x1)
-x3 = Dense(30, activation='relu')(x2)
-x4 = Dense(15, activation='softmax')(x3)
-
-output_layer = Dense(7, activation='sigmoid')(x4)
-
-mod = Model(inputs=input_layer, outputs=output_layer)
-
-print("network made, now compiling")
-# compile the neural network
-mod.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
-'''
  
 
 #Extremely Randomized Trees
-classifier = XTC(	
+classifier = ERT(	
 					n_estimators = 200,					# Number of weak learners in the forest (Decision Trees)
 					max_features =  "sqrt",				# Max number of features to consider while attempting classification
 					min_samples_split =  2,				# Minimum number of features required to make further splits, # < 2 is a leaf.
@@ -62,23 +34,11 @@ classifier = XTC(
 				)
 
 print('compiled, now running')
-# train the neural network
-#history = mod.fit(x_train, y_train, 32, 300, validation_split=0.2)
+
+# Fitting
 fit = classifier.fit(x_train, y_train)
 
-print("running score func")
-
+print("running score function")
 acc = classifier.score(x_test, y_test)
+print("Accuracy: ", acc)
 
-print(acc)
-
-'''
-# evaluate for success
-train_loss, train_acc = mod.evaluate(x_train, y_train)
-test_loss, test_acc = mod.evaluate(x_test, y_test)
-
-print("Training set accuracy: ", train_acc)
-print("Test set accuracy: ", test_acc)
-
-
-'''
